4bd98b8c18cd96589575c873e44a59ac38b33677
Kenneth <knthyang@gmail.com>
2026-02-25 20:09:45 -0500

    cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './env/.env' });
-
 import express from 'express';
+import cors from 'cors';
+
 import notesRoutes from './routes/notes.routes.js';
 import {connectDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5001;
 const app = express(); // initialize web server
 
 //middleware
+app.use(cors());
 app.use(express.json()); // allows us to get access to req.body 
 // app.use(rateLimiter);
 
